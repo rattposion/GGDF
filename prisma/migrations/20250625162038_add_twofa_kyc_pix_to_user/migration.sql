501b08a111ce5aab161b09845f5d6e5013ee49f4
Wesley <142677140+rattposion@users.noreply.github.com>
2025-06-27 10:34:12 -0300

    Delete prisma directory

--- prisma/migrations/20250625162038_add_twofa_kyc_pix_to_user/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "kyc" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "pix" TEXT,
-ADD COLUMN     "twofa" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250625224522_add_emoji_to_subcategory/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Subcategory" ADD COLUMN     "emoji" TEXT;
