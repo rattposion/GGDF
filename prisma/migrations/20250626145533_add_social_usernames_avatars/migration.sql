be3df9f9d1307a63c8c4908ab2a11bcf54804580
Wesley <142677140+rattposion@users.noreply.github.com>
2025-06-28 19:41:15 -0300

    Delete prisma directory

--- prisma/migrations/20250626145533_add_social_usernames_avatars/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "discordAvatar" TEXT,
-ADD COLUMN     "discordUsername" TEXT,
-ADD COLUMN     "steamAvatar" TEXT,
-ADD COLUMN     "steamUsername" TEXT;
